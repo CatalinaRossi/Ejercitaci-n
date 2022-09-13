2b79055c319cd0b63072007742e5ca40e5c2bba7
CatalinaRossi <catalinaarossi@gmail.com>
2022-09-13 10:12:53 -0300

    Ejercicio 3

---
+++ Clase_1/ejercicio_3.py
@@ -0,0 +1,6 @@
+palabra = input("Sr. Usuario, por favor, ingrese una palabra: ")
+longitud_palabra = len(palabra)
+#print(longitud_palabra)
+asteriscos = "*" * longitud_palabra
+#print(asteriscos)
+print(palabra, " ", asteriscos)
